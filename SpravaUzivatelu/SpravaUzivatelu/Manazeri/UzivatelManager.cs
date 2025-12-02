5b7c0677c4de999ade4a8361608869227cf1ea43
LenkaSimkova <c23simkova@oakostelec.cz>
2025-12-02 10:52:02 +0100

    uzivatel manager update

--- SpravaUzivatelu/SpravaUzivatelu/Manazeri/UzivatelManager.cs
+++ SpravaUzivatelu/SpravaUzivatelu/Manazeri/UzivatelManager.cs
@@ -4,19 +4,25 @@ using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace SpravaUzivatelu.Manazeri
 {
     internal class UzivatelManager
     {
+        //ukládá všechny uživatele v paměti - spravuje to manager (registrace, přihlášení, admin)
         private List<Uzivatel> users;
 
-        public UzivatelManager()
+        public UzivatelManager() //spustí se automaticky při vytvoření účtu. 
         {
+            //načte uživatele ze souboru
             users = IOManager.LoadUsers();
 
+
+            //zkontroluje, jestli už existuje admin
             if (!users.Any(u => u.Role == "admin"))
             {
+                //Pokud není v seznamu žádný admin, vytvoří se automaticky
                 users.Add(new Uzivatel
                 {
                     Name = "admin",
@@ -25,45 +31,57 @@ namespace SpravaUzivatelu.Manazeri
                     Registered = DateTime.Now
                 });
                 IOManager.SaveUsers(users);
-            }
+            } //aplikace má vždy minimálně jednoho administrátora
         }
 
         public bool Register(string name, string password, string role = "user")
+            //umožní vytovření novýho uživatle
         {
-            if (string.IsNullOrWhiteSpace(name) || string.IsNullOrWhiteSpace(password)) return false;
-            if (users.Any(u => u.Name.Equals(name, StringComparison.OrdinalIgnoreCase)))
+            if (string.IsNullOrWhiteSpace(name) || string.IsNullOrWhiteSpace(password)) return false; //Uživatel musí mít jméno i heslo
+            if (users.Any(u => u.Name.Equals(name, StringComparison.OrdinalIgnoreCase))) //Jméno nesmí být použito dvakrát
                 return false;
 
+
+            //Vytvoření instance nového uživatele
             var um = new Uzivatel
             {
                 Name = name,
                 PasswordHash = Hash(password),
                 Role = role,
                 Registered = DateTime.Now
+                //heslo se ukládá zahashované, ne v čitelné podobě
+                 //uloží se datum registrace
             };
 
+            //Uložení do seznamu a do souboru
             users.Add(um);
             IOManager.SaveUsers(users);
-            return true;
+            return true; //Vrací true, pokud je registrace úspěšná
         }
 
-        public Uzivatel Login(string name, string password)
-        {
+        public Uzivatel Login(string name, string password) //přihlášení uživatele
+
+        {//Vyhledá uživatele podle jména (bez rozlišení velkých a malých písmen)
+            //Porovná hash uloženého hesla s hashem hesla, které zadal uživatel
             return users.FirstOrDefault(u =>
                 u.Name.Equals(name, StringComparison.OrdinalIgnoreCase) &&
                 u.PasswordHash == Hash(password));
+
+            //Vrátí objekt uživatele, pokud přihlášení proběhlo správně.
+            //Vrátí null, pokud je jméno nebo heslo špatně.
         }
 
-        private string Hash(string input)
-        {
-            using (var sha = SHA256.Create())
+        private string Hash(string input) //vytvoření hashování
+        {//hashovací algoritmus
+
+            using (var sha = SHA256.Create()) //Vytvoří SHA256 hashovací algoritmus
             {
-                var bytes = sha.ComputeHash(Encoding.UTF8.GetBytes(input));
-                return BitConverter.ToString(bytes).Replace("-", "").ToLowerInvariant();
+                var bytes = sha.ComputeHash /*Vypočítá has (ten sha.ComputeHash)*/(Encoding.UTF8.GetBytes(input)); //převede to na bajty
+                return BitConverter.ToString(bytes).Replace("-", "").ToLowerInvariant(); //heslo se nikdy neukládá v čitelné formě, pouze jako hash.
             }
             
         }
-
+        //Vrátí celý seznam uživatelů, je to užitečný třeba pro administráta, který sei chce zobrazit registrovaný uživatelé
         public List<Uzivatel> GetAll() => users;
     }
 }
