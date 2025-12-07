a5cd3b5aaa9317d6f50b50028afb5d565f3e649d
Hulkys <c23hulka@oakostelec.cz>
2025-12-07 10:31:06 +0100

    save

--- SpravaUzivatelu/SpravaUzivatelu/Login.Designer.cs
+++ SpravaUzivatelu/SpravaUzivatelu/Login.Designer.cs
@@ -107,11 +107,11 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(4, -8);
+            this.pictureBox1.Location = new System.Drawing.Point(-11, -8);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(792, 467);
+            this.pictureBox1.Size = new System.Drawing.Size(826, 480);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 16;
+            this.pictureBox1.TabIndex = 18;
             this.pictureBox1.TabStop = false;
             // 
             // Login
