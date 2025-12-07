23be3632012f6465e8f12cda50d4fa771459bf3e
Hulkys <c23simkova@oakostelec.cz>
2025-12-07 10:39:22 +0100

    omyl

--- SpravaUzivatelu/SpravaUzivatelu/Manazeri/ActionManager.cs
+++ SpravaUzivatelu/SpravaUzivatelu/Manazeri/ActionManager.cs
@@ -14,5 +14,33 @@ namespace SpravaUzivatelu.Manazeri
         {
             userManager = um;
         }
+        public bool ChangePassword(Uzivatel user, string newPassword) //nacte usera a nove heslo
+        {
+            if (user == null || string.IsNullOrWhiteSpace(newPassword)) return false; //pokud uzivatel neexistuje nebo napsat nic do toho nenastavi heslo
+
+            // SHA
+            using (var sha = System.Security.Cryptography.SHA256.Create())
+            {
+                var bytes = sha.ComputeHash(System.Text.Encoding.UTF8.GetBytes(newPassword));
+                user.PasswordHash = BitConverter.ToString(bytes).Replace("-", "").ToLower();
+            }
+            IOManager.SaveUsers(userManager.GetAll());//uloží seznam s novým heslem
+            return true;//finito
+        }
+        public bool DeleteUser(Uzivatel admin, string name) //smaze uzivatele
+        {
+            if (admin == null) return false; //pokud nemá žádnou rolu
+            if (!admin.Role.Equals("admin")) return false; //pokud má roli "admin"
+            //DÁL SE DOSTANE JEN ADMIN UŽ
+            var users = userManager.GetAll(); //vezme všechny uživatele v JSONu
+            var vybrany = users.FirstOrDefault(u => u.Name.Equals(name, System.StringComparison.OrdinalIgnoreCase));//koukne jestli je nějaký uživ. s tím jménem
+            if (vybrany == null) return false; //pokud neexistuje, tak se nic nesmaže
+            if (vybrany.Role == "admin") return false;//pokud role vybraneho uživatele ke smazání je admin, nesmaže se
+
+            users.Remove(vybrany);//odebere vybraneho uživatele
+            IOManager.SaveUsers(users);//uloží nově seznam bez danného uživatele
+            return true;//finito
+        }
+
     }
 }
